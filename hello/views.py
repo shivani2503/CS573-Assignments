193a34869fee86d7fcd3c8e25db1658256a01058
SHIVANI <shivi0193.usa@gmail.com>
2019-12-21 16:41:06 -0700

    Heroku assignment

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,3 @@
-import requests
-
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -8,9 +6,7 @@ from .models import Greeting
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    return render(request, "index.html")
 
 
 def db(request):
